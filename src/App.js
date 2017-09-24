30d10d3eb99d76ec5859503a619637b8ce15c98e
Tyler-Losinski <tyler.a.losinski@ndsu.edu>
2017-09-23 22:42:11 -0500

    WIP Filters now function

--- src/App.js
+++ src/App.js
@@ -2,18 +2,79 @@ import React, { Component } from 'react';
 import { Card, Menu, Grid, Item, Image, Dropdown} from 'semantic-ui-react'
 //import { Legend, PieChart } from 'react-easy-chart';
 import logo from './CF_Logo.png';
-import data from './Career Fair.json'
+import masterData from './Career Fair.json'
 import './App.css';
 
-const majors = [ { key: 'Agricultural & Biosystems Engineering', value: 'Agricultural & Biosystems Engineering', text: 'Agricultural & Biosystems Engineering' }, ]
+const majors = [ { key: 'Agricultural & Biosystems Engineering', value: 'Agricultural & Biosystems Engineering', text: 'Agricultural & Biosystems Engineering' },
+ ]
 
 class App extends Component {
 
-  state = { activeItem: 'Companies',
-            companies: data.companies
-}
+  constructor(props) {
+    super(props);
+
+    var majors = [];
+    var state = [];
+    var degree = [];
+    var positionType = [];
+    var sponsorships = [];
+    var interviewing = [];
+
+    for(var i = 0; i<masterData.filters.length;i++){
+      switch(masterData.filters[i].field_name){
+        case 'degree_levels':
+          degree.push({key: masterData.filters[i].value, value: masterData.filters[i].value, text: masterData.filters[i].value,});
+          break;
+        case 'field_3':
+          state.push({key: masterData.filters[i].value, value: masterData.filters[i].value, text: masterData.filters[i].value,});
+          break;
+        case 'field_5':
+          interviewing.push({key: masterData.filters[i].value, value: masterData.filters[i].value, text: masterData.filters[i].value,});
+          break;
+        case 'majors':
+          majors.push({key: masterData.filters[i].value, value: masterData.filters[i].value, text: masterData.filters[i].value,});
+          break;
+        case 'position_types':
+          positionType.push({key: masterData.filters[i].value, value: masterData.filters[i].value, text: masterData.filters[i].value,});
+          break;
+        case 'work_authorizations':
+          sponsorships.push({key: masterData.filters[i].value, value: masterData.filters[i].value, text: masterData.filters[i].value,});
+          break;
+
+      }
+    }
+
+    this.state = {
+      activeItem: 'Companies',
+      companies: masterData.companies,
+      majors: majors,
+      state: state,
+      degree: degree,
+      positionType: positionType,
+      sponsorships: sponsorships,
+      interviewing: interviewing,
+      filters: []
+    };
+  }
 
   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  filterChanged = (e, data) =>{
+    var filteredData = []
+    for(var i = 0;i< masterData.companies.length;i++){
+      for(var j = 0; j < data.value.length; j++){
+        if(masterData.companies[i][data.name].indexOf(data.value[j]) > -1){
+          filteredData.push(masterData.companies[i]);
+          break;
+        }
+      }
+    }
+
+    this.setState({
+      companies:filteredData
+    });
+
+  }
 
   render() {
     const { activeItem } = this.state;
@@ -24,7 +85,6 @@ class App extends Component {
           <h2>Welcome to Career Fair Info</h2>
           <br />
         </div>
-        <p className="App-intro">
           <Grid relaxed columns={3} style={{ padding: 10 }}>
             <Grid.Row>
               <Grid.Column width={3}>
@@ -66,24 +126,18 @@ class App extends Component {
                   <Card.Content header='Filters' />
                   <Card.Content >
                     <Item.Group>
-                    Major: <Dropdown placeholder='Major' search selection options={majors} onChange={(e,data)=>{
-                      var filteredData = []
-                        for(var i =0;i<this.state.companies.length;i++){
-                          if(this.state.companies[i].majors.indexOf(data.value) > -1)
-                            filteredData.push(this.state.companies[i]);
-                        }
-                        this.setState({
-                          companies: filteredData
-                        });
-                        console.log(data.value);
-                      }} />
+                    Major: <Dropdown name='majors' placeholder='major' search selection multiple options={this.state.majors} onChange={this.filterChanged} />
+                    Degree: <Dropdown name='degree_levels' placeholder='Degree' search selection multiple options={this.state.degree} onChange={this.filterChanged} />
+                    Interviewing: <Dropdown name='field_5' placeholder='Interviewing' search selection multiple options={this.state.interviewing} onChange={this.filterChanged} />
+                    Location: <Dropdown name='field_3' placeholder='State' search selection multiple options={this.state.state} onChange={this.filterChanged} />
+                    Sponsorships?: <Dropdown name='work_authorizations' placeholder='Sposorship' search selection multiple options={this.state.sponsorships} onChange={this.filterChanged} />
+                    Position Type: <Dropdown name='position_types' placeholder='Position Type' search selection multiple options={this.state.positionType} onChange={this.filterChanged} />
                     </Item.Group>
                   </Card.Content>
                 </Card>
               </Grid.Column>
             </Grid.Row>
           </Grid>
-        </p>
       </div>
     );
   }
